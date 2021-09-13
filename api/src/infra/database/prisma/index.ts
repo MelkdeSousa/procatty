fe5f77fff7f74153222e555efc36d8ee8959cb1c
melk_de_sousa <melkindesousa@gmail.com>
2021-09-13 00:48:30 -0300

    chore: interfaces of Entities from prisma

--- api/src/infra/database/prisma/index.ts
+++ api/src/infra/database/prisma/index.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Address, Professional, Admin, Schedule } from '@prisma/client';
 
 export const { professional, schedule, admin, address } = new PrismaClient()
+
+export interface IProfessional extends Professional { }
+export interface IAddress extends Address { }
+export interface IAdmin extends Admin { }
+export interface ISchedule extends Schedule { }
