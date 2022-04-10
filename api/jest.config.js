7788b0ca39d630de10884f84e988617a4ba93b0f
MelkdeSousa <melk.sousap@gmail.com>
2022-04-10 12:40:30 -0300

    chore(jest): config path mappings in jest

--- api/jest.config.js
+++ api/jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   maxWorkers: "50%",
+  setupFiles: ["./jest.setup.js"],
+  moduleNameMapper: {
+    "^@src/(.*)$": "<rootDir>/src/$1",
+    "^@core/(.*)$": "<rootDir>/src/core/$1",
+    "^@infra/(.*)$": "<rootDir>/src/infra/$1",
+    "^@app/(.*)$": "<rootDir>/src/app/$1",
+  },
 };
