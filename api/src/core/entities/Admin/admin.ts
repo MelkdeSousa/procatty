f182d3d5f3b5e52b2e72748193f0a0730d361f27
MelkdeSousa <melk.sousap@gmail.com>
2022-04-10 13:15:26 -0300

    refact(core/entities/admin): refactore validation

--- api/src/core/entities/Admin/admin.ts
+++ api/src/core/entities/Admin/admin.ts
@@ -1,41 +1,13 @@
-import { PASSWORD_STRONG, USERNAME } from '../../constants/regex'
-import { z as zod } from 'zod'
-
-import { Entity } from '../entity'
+import { Entity } from '@core/entities/entity'
+import { AdminValidator } from './adminValidator'
 
 export interface IAdminProps {
   name: string
   email: string
-  avatar: string
+  avatar?: string
   username: string
   password: string
 }
-
-export const adminSchemaValidation = zod.object({
-  _id: zod
-    .string()
-    .uuid()
-    .optional(),
-  name: zod
-    .string()
-    .nonempty({ message: 'name is required' })
-    .min(2, { message: 'name must have at least 2 characters' }),
-  email: zod
-    .string()
-    .email({ message: 'email is invalid' }),
-  avatar: zod
-    .string()
-    .url({ message: 'url to avatar is invalid' }),
-  username: zod
-    .string()
-    .min(6, { message: 'username must have at least 6 characters' })
-    .regex(USERNAME, { message: 'username is invalid' }),
-  password: zod
-    .string()
-    .min(6, { message: 'password must be at least 6 characters' })
-    .max(128, { message: 'password is too long' })
-    .regex(PASSWORD_STRONG, { message: 'password must have at least one uppercase, one lowercase, one number and one special character' })
-})
 
 export class Admin extends Entity {
   private props: IAdminProps;
@@ -52,28 +24,19 @@ export class Admin extends Entity {
     return this.props.username
   }
 
-  get avatar(): string {
+  get avatar(): string | undefined {
     return this.props.avatar
   }
 
-  constructor(props: IAdminProps, id?: string) {
-    super(id)
-    if (this.isValid(props)) {
-      this.props = props
-    }
-
-    if (super.errors.length > 0) {
-      throw new Error(`${this._errors[0].field}: ${this._errors[0].message}`)
-    }
+  get password(): string {
+    return this.props.password
   }
 
-  public isValid(props: IAdminProps) {
-    const isValid = adminSchemaValidation.safeParse(props)
+  constructor(id: string, props: IAdminProps) {
+    super()
+    this._id = id
+    this.props = props
 
-    if (!isValid.success) {
-      isValid.error.errors.map((error) => this._errors.push({ message: error.message.toLowerCase(), field: error.path.join('.').toLowerCase() }))
-    }
-
-    return isValid.success
+    this.validate<IAdminProps>(props, new AdminValidator())
   }
 }
