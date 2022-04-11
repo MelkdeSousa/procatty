d87fb71f67af1a7ec20bfa9557b4caca822d3fd3
MelkdeSousa <melk.sousap@gmail.com>
2022-04-11 09:07:27 -0300

    feat(infra/repository/memory/admin): implementation repository in memory for admin entity

---
+++ api/src/infra/repository/memory/admin.ts
@@ -0,0 +1,22 @@
+import { IAdminProps } from "@core/entities/Admin";
+import { IAdminRepository } from "@core/interfaces/repositories/admin";
+
+export class MemoryRepository implements IAdminRepository {
+
+  private admins: ({ id: string } & IAdminProps)[] = [];
+
+  public async create(id: string, admin: IAdminProps): Promise<void> {
+    this.admins.push({
+      id,
+      ...admin
+    })
+  }
+
+  public async exists(email: string): Promise<boolean> {
+    return this.admins.some(admin => admin.email === email);
+  }
+
+  public async findByEmail(email: string): Promise<IAdminProps | null> {
+    return this.admins.find(admin => admin.email === email) || null;
+  }
+}
