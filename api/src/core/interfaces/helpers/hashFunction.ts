99764575acaae9f8015a5372abe90c893d367ac4
MelkdeSousa <melk.sousap@gmail.com>
2022-04-15 08:46:28 -0300

    docs(helpers/hash-function): add docstring

--- api/src/core/interfaces/helpers/hashFunction.ts
+++ api/src/core/interfaces/helpers/hashFunction.ts
@@ -1,3 +1,4 @@
+/* Defining the interface for a class that will be used to hash and compare values. */
 export interface IHashFunction {
   execute(value: string): Promise<string>
   compare(value: string, hash: string): Promise<boolean>
