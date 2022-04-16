ba51ca650f9e7d9289fe4eb16fd71767061faf6e
Melquisedeque de Sousa Pereira <melquisedeque.pereira@sigtrans.com.br>
2022-04-16 15:20:26 -0300

    docs(routes/admin): doc endpoint of create admin

--- api/src/infra/http/routes/admin.routes.ts
+++ api/src/infra/http/routes/admin.routes.ts
@@ -6,6 +6,56 @@ import { makeCreateAdminController } from '@infra/http/factories/controller/make
 
 const adminRouter = express.Router()
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      Admin:
+ *        type: object
+ *        required:
+ *          - name
+ *          - email
+ *          - password
+ *          - username
+ *        properties:
+ *          name:
+ *            type: string
+ *          email:
+ *            type: string
+ *          password:
+ *            type: string
+ *          username:
+ *            type: string
+ *          avatar:
+ *            type: string
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Admin
+ *   description: Admin management
+ */
+
+/**
+ * @swagger
+ * /admin:
+ *   post:
+ *     summary: Create an admin
+ *     tags: [Admin]
+ *     description: Create an admin
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Admin'
+ *     responses:
+ *       201:
+ *         description: Created
+ *       400:
+ *         description: Bad Request
+ */
 adminRouter.post('/', adaptRoute(makeCreateAdminController()))
 
 export { adminRouter }
