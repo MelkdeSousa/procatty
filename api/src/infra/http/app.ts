790994c1643b2bb47832ab51939e1a20478c9091
Melquisedeque de Sousa Pereira <melquisedeque.pereira@sigtrans.com.br>
2022-04-16 15:21:24 -0300

    feat(server): add doc api with swagger

--- api/src/infra/http/app.ts
+++ api/src/infra/http/app.ts
@@ -1,12 +1,30 @@
 import cors from 'cors'
 import { config } from 'dotenv-flow'
 import express from 'express'
+import swaggerUI from 'swagger-ui-express'
+import swaggerJSDoc, { Options } from 'swagger-jsdoc'
 
 config({ silent: true })
 
 import { router } from './routes'
+import { resolve } from 'path'
 
 const app = express()
+
+const options: Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Procatty API',
+      version: '1.0.0',
+    },
+  },
+  apis: [resolve(__dirname, 'routes', '*.routes.ts')],
+};
+
+const spec = swaggerJSDoc(options);
+
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(spec))
 
 app.use(
   cors({
@@ -21,5 +39,6 @@ app.use(
 )
 
 app.use(router)
+
 
 export { app }
